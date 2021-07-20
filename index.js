94891e39fed1d888e689ee8516066eceb84bf1b5
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2021-07-20 10:39:34 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Animated } from 'react-native';
+
+const LoadingSwitch = (props) => {
+
+
+    <Animated.View>
+
+    </Animated.View>
+}
+
+export default LoadingSwitch;
